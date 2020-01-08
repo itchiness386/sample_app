dbe3927484ddd9c215ea13e8435a53440bd28296
itchiness386 <itchiness386@gmail.com>
2020-01-08 15:41:22 +0900

    [Finish] 3-4

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_055420) do
+ActiveRecord::Schema.define(version: 2020_01_08_063839) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
